9ba9a5f460766796f6ba1b708a97eb4b5217da49
yhostc <yhostc@gmail.com>
2012-12-11 10:44:52 +0800

    完成IP查询修改

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
 var lbs = require('./index.js');
 
+var ips = [
+	'127.0.0.1',
+	'114.247.50.32',
+	'42.121.18.30',
+	'116.255.159.28',
+	'175.41.22.214',
+	'113.46.75.217'
+];
+
 var t1 = new Date();
 
-console.log(lbs.query('127.0.0.1'));
-
-console.log(lbs.query('114.247.50.32'));
-
-console.log(lbs.query('42.121.18.30'));
-
-
-console.log(lbs.query('116.255.159.28'));
-
-
-console.log(lbs.query('175.41.22.214'));
-
-
-console.log(lbs.query('113.46.75.217'));
-
+for(var i=0; i<ips.length; i++){
+	console.log(lbs.query(ips[i]));
+}
 
 console.log('time:'+(new Date()-t1));
