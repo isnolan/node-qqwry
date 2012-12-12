34eb08489b2bd37d191d6b0bd09ff721cf3de942
yhostc <yhostc@gmail.com>
2012-12-12 21:12:43 +0800

    关联数据
    将IP数据库返回数据作出处理，以便于和中国行政区划数据挂钩

---
+++ batch/batch.js
@@ -0,0 +1,81 @@
+var fs = require('fs'),
+    util = require('util'),
+    http = require('http'),
+	events = new require("events"),
+	iconv = require('iconv-lite');
+	
+// 定义搜索类
+function query(keyword){
+	events.EventEmitter.call(this); 
+	var that = this;
+	var options = {
+		host : '10.2.134.39',
+		port : 80,
+		path : '/geocode/simple?resType=json&encode=utf-8&sid=7000&address='+keyword,
+		method : 'GET'
+	};
+	var req = http.request(options, function(res) {
+		//console.log('STATUS: ' + res.statusCode);
+		var buf = new Buffer(0);
+		res.on('data', function (chunk) {
+			var newBuf = new Buffer(buf.length+chunk.length);
+			buf.copy(newBuf, 0);
+			chunk.copy(newBuf, buf.length);
+			buf = newBuf;
+		});
+		res.on('end', function(){
+			that.emit('complete', JSON.parse(buf.toString()));
+			//console.log(buf.toString());
+		});
+	});
+	req.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
+	req.end();
+}
+util.inherits(query, events.EventEmitter);
+
+
+//加载ip.txt文件，读取并分析所有地址，并且排重
+var txt = fs.readFileSync('../data/ip.txt'),
+	txt = iconv.decode(txt, 'GBK'),
+	rows = txt.split("\n");
+	
+var address = [], idx = 0;
+for(var i=0; i<rows.length; i++){
+	row = rows[i].split(/\s+/g);
+	if(row[2] && address.indexOf(row[2])<0){
+		address.push(row[2]);
+	}
+}
+console.log('排重后还有：'+address.length);
+
+
+var relations = {}, errors = [], j=0;;
+// 批量进行地理解析
+
+
+function batch(keywords){
+	var s = new query(keywords);
+	s.on('complete',function(data){
+		if(data.status!='E0'){
+			errors.push(keywords);
+		}else{
+			var poi = data.list[0];
+			relations[keywords] = (poi.city || poi.province);
+			j++;
+		}
+		if(idx<address.length){
+			idx++;
+			batch(address[idx]);
+		}else{
+			fs.writeFileSync('../data/relation.js', JSON.stringify(relations));
+			
+			console.log('error for:'+errors.length+', ->'+errors.join(","));
+			console.log('success for: '+j);
+		}
+	});
+	console.log('batch for :'+address.length+'-'+idx+', '+keywords);
+}
+batch(address[idx]);
+
